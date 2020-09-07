8133d98ce22040b0c2dd1efad83ee5d47d940a72
Janetxxx <41528500+Janetxxx@users.noreply.github.com>
2020-09-08 00:16:06 +1000

    Update lesson_6.js

--- lesson_6.js
+++ lesson_6.js
@@ -1,8 +1,16 @@
-//2d means two-demension graph
-//in this way, create a small yellow squre
+// 2d means two-demension graph
+// in this way, create a small yellow square
 
 var ctx = dd1.getContext("2d");
-ctx.fillStyle='yellow';
+ctx.fillStyle = "yellow";
 ctx.fillReact(10,10,40,40);
+
+// add one more yellow square next to it
+ctx.fillReact(60,10,40,40);
+
+// add text in black
+ctx.fillStyle = "black";
+ctx.font = "50px Arial";
+ctx.fillText("Hello", 10, 80);
 
 
